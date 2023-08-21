28151731acacb173637f85c19056acac84ce2cd1
Tayyab Ali <sta9106@gmail.com>
2023-08-21 22:44:19 +0500

    initial commit

--- FrameworkTestingApp/ContentView.swift
+++ FrameworkTestingApp/ContentView.swift
@@ -6,6 +6,9 @@
 //
 
 import SwiftUI
+import DataFramework
+import Alamofire
+import RealmSwift
 
 struct ContentView: View {
     var body: some View {
@@ -16,8 +19,60 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            let logger = VersionLogger()
+            /*
+            logger.versionInfo()
+            logger.printCustomEncoding()
+            logger.getCategories { data, error in
+                print(data)
+                print(error)
+            }
+            
+            AF.request("https://api.beautygo.it/api/categories/macro").responseString { response in
+                switch response.result {
+                case .success(let data):
+                    print("Data:" + data)
+                case .failure(let error):
+                    print("Error: \(error)")
+                }
+            }*/
+            
+            /*
+            do {
+                try logger.createDB(schemaVersion: 1)
+                try logger.addUser(name: "User105")
+                let users = logger.getUser()
+                print(users)
+                
+            } catch {
+                print("DB Error: \(error)")
+            } */
+            
+            do {
+                let config = Realm.Configuration(schemaVersion: 3)
+                let realm = try Realm(configuration: config)
+                
+                let user = UserB()
+                user.username = "Mad Boy"
+                try realm.write{
+                    realm.add(user)
+                }
+                
+                let users = realm.objects(UserB.self)
+                print(users)
+                
+            } catch {
+                print("DB Error: \(error)")
+            }
+        }
     }
 }
+
+class UserB: Object {
+    @objc dynamic var username = ""
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
